e09055095b9df3c28fd04a1468622236dd4d4eb9
Mark <voronkov1mark@gmail.com>
2023-07-13 01:44:47 +0300

    Initial commit

---
+++ src/test/java/com/example/domashka_osnovy_algoritmov1/DomashkaOsnovyAlgoritmov1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.domashka_osnovy_algoritmov1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomashkaOsnovyAlgoritmov1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
